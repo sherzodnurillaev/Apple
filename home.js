ec5b668f93b61dd8d2e75cc52ed706d251684a77
Amal <sherzod1700@mail.ru>
2024-05-11 00:23:19 +0500

    Мадальное окно не готова

--- home.js
+++ home.js
@@ -1,67 +1,50 @@
+const btns = document.querySelectorAll('.color div')
+const img = document.querySelector('.transition_pho img')
+const links = document.querySelectorAll('.transition_pho button')
+const pers = document.querySelectorAll('.transition_pho div')
+const exe = document.querySelector('.exe')
 
-const mid = document.querySelector('.midBut')
-let pho = document.querySelector('.pho')
-let but = document.querySelector('.imgBut')
-let per_o = document.querySelector('.per_one')
-let per_t = document.querySelector('.per_two')
+const di = document.querySelector('.box a')
 
-let white = document.querySelector('.white')
-let black = document.querySelector('.black')
-let blue = document.querySelector('.blue')
-let gold = document.querySelector('.gold')
 
-mid.onclick = () => {
-    if (pho.getAttribute('src') === './images/home/img_1.webp') {
-        pho.setAttribute('src', './images/home/img_2.webp')
-        mid.setAttribute('class', 'midBut_')
-        but.setAttribute('src', './images/logo/arrow_left.png')
-        per_o.setAttribute('class', 'per_one_')
-        per_t.setAttribute('class', 'per_two_')
-        
-    } else {
-        pho.setAttribute('src', './images/home/img_1.webp')
-        mid.setAttribute('class', 'midBut')
-        but.setAttribute('src', './images/logo/arrow.png')
-        per_o.setAttribute('class', 'per_one')
-        per_t.setAttribute('class', 'per_two')
-    }
+const iphones = {
+    white: 'https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-1inch-whitetitanium?wid=5120&hei=2880&fmt=webp&qlt=70&.v=VW44dkRidm5wazhwcGxtL0cyaEJ2VTkrNXBUdUJSK1k4NE5seUtJaW80ZE9GbVRLdFoyOVBmczRNaU91Q1BaNWlCQmV2WTA2cncybDF2YzFnKzI0S2prMCtUNWwzYWR0UVU3TWVsbEdUeXRjODhrWk5XcFl2eGdtMU93TW5UemN5bURtdG84aElEZERZa0lIV3FCN1lBPT0=&traceId=1',
+    black:  'https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-1inch-blacktitanium?wid=5120&hei=2880&fmt=webp&qlt=70&.v=VW44dkRidm5wazhwcGxtL0cyaEJ2VTkrNXBUdUJSK1k4NE5seUtJaW80ZW95OFpIcWQ1d0NuTUppaGxOVkdhQWlCQmV2WTA2cncybDF2YzFnKzI0S2prMCtUNWwzYWR0UVU3TWVsbEdUeXRjODhrWk5XcFl2eGdtMU93TW5UemNnaGZTejMxa0hnUE1YYTBGeVJZdVBBPT0=&traceId=1',
+    blue:   'https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-1inch-bluetitanium?wid=5120&hei=2880&fmt=webp&qlt=70&.v=VW44dkRidm5wazhwcGxtL0cyaEJ2VTkrNXBUdUJSK1k4NE5seUtJaW80Y1JhMkZic05CWW9VU1dxSWZUUWVyT3B3azBNWWRMTTJUR1Y1SkFQMTJmY3dDb1F2RTNvUEVHRkpGaGtOSVFHak1BVzRkYUlmdElTYk5KZFpJVWpSSncvTitpdit2K0pPbnlhL04zRlVpb29BPT0=&traceId=1',
+    gold:   'https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-1inch-naturaltitanium?wid=5120&hei=2880&fmt=webp&qlt=70&.v=VW44dkRidm5wazhwcGxtL0cyaEJ2VTkrNXBUdUJSK1k4NE5seUtJaW80ZEk5aWVjRmFBS2tnWEF6QzlCMm5HL0pOZTBYalh5Vk90cEc1K2wwRzFGejRMeXJHUnUva2huMjl4akFHOXNwVjA0YXFmK3VWSWZuRE9oVEFyUFR0T2hBa0RRdWFDUTBBczVnS0JqV3BGbkxRPT0=&traceId=1',
 }
 
-white.onclick = () => {
-    pho.setAttribute('src', './images/home/img_1.webp')
-    // pho.setAttribute('src', './images/home/white.webp')
-    if (pho.getAttribute('src') === './images/home/img_1.webp') {
-        pho.setAttribute('src', './images/home/white.webp')
-    } else {
-        pho.setAttribute('src', './images/home/img_1.webp')
-    }
-}
-black.onclick = () => {
-    pho.setAttribute('src', './images/home/img_1.webp')
-    // pho.setAttribute('src', './images/home/white.webp')
-    if (pho.getAttribute('src') === './images/home/img_1.webp') {
-        pho.setAttribute('src', './images/home/black.jpeg')
-    } else {
-        pho.setAttribute('src', './images/home/img_1.webp')
-    }
-}
-blue.onclick = () => {
-    pho.setAttribute('src', './images/home/img_1.webp')
-    // pho.setAttribute('src', './images/home/white.webp')
-    if (pho.getAttribute('src') === './images/home/img_1.webp') {
-        pho.setAttribute('src', './images/home/blue.png')
-    } else {
-        pho.setAttribute('src', './images/home/img_1.webp')
-    }
-}
-gold.onclick = () => {
-    pho.setAttribute('src', './images/home/img_1.webp')
-    // pho.setAttribute('src', './images/home/white.webp')
-    if (pho.getAttribute('src') === './images/home/img_1.webp') {
-        pho.setAttribute('src', './images/home/gold.png')
-    } else {
-        pho.setAttribute('src', './images/home/img_1.webp')
-    }
+const linke = {
+    midBut: 'https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-15-pro-model-unselect-gallery-2-202309_GEO_US?wid=5120&hei=2880&fmt=webp&qlt=70&.v=UW1GeTRObi9UaVF4S3FUNERNMWVhZ2FRQXQ2R0JQTk5udUZxTkR3ZVlpS0o0bnJBQlJYRTdzdWVwMVBVb2c4L0lYUWYrQkRLNitCbE9QRVRqNHErMkE3b3pFWnhZZ2g0M0pRR0pEdHVSRUcyRlVVa0JFTnZqc0lHcUFYQnFjNXpkc3NlSXRDWlQ3WVl5dEd4ZUF1dDFRPT0=&traceId=1',
+    midBut_: 'https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-15-pro-model-unselect-gallery-1-202309?wid=5120&hei=2880&fmt=webp&qlt=70&.v=UW1GeTRObi9UaVF4S3FUNERNMWVhZ2FRQXQ2R0JQTk5udUZxTkR3ZVlpSmVJdk5rWHR5c3l5ME9ZNVV1Y1o0SjBoUVhuTWlrY2hIK090ZGZZbk9HeE1xUVVnSHY5eU9CcGxDMkFhalkvT0Q5QmVFZ0s0ZTVTcDJpVVJkaDNNVDdmbW94YnYxc1YvNXZ4emJGL0IxNFp3PT0=&traceId=1',
 }
 
-// 976151485
+btns.forEach(btn => {
+    btn.onclick = () => {
+        const color = btn.className.toLowerCase()
+        img.setAttribute('src', iphones[color])
+        links.forEach(link => {
+            link.style.display = 'none'
+        })
+        exe.style.display = 'block'
+    }
+})
+
+links.forEach(link => {
+    link.onclick = () => {
+        const imags = link.className
+        img.setAttribute('src', linke[imags])
+    }
+})
+
+exe.onclick = () => {
+    img.setAttribute('src', 'https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-15-pro-model-unselect-gallery-1-202309?wid=5120&hei=2880&fmt=webp&qlt=70&.v=UW1GeTRObi9UaVF4S3FUNERNMWVhZ2FRQXQ2R0JQTk5udUZxTkR3ZVlpSmVJdk5rWHR5c3l5ME9ZNVV1Y1o0SjBoUVhuTWlrY2hIK090ZGZZbk9HeE1xUVVnSHY5eU9CcGxDMkFhalkvT0Q5QmVFZ0s0ZTVTcDJpVVJkaDNNVDdmbW94YnYxc1YvNXZ4emJGL0IxNFp3PT0=&traceId=1')
+    links.forEach(link => {
+        link.style.display = 'block'
+    })
+    exe.style.display = 'none'
+}
+
+di.onclick = () => {
+    ShowModel('dialog')
+}
